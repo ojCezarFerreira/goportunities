bb8d4fd4376e773994779f21a7e1686249f31af8
Julio Cezar Leão Ferreira <cezarleferr@gmail.com>
2025-07-20 15:00:26 -0400

    docs: add documentation for delete opening

--- handler/deleteOpening.go
+++ handler/deleteOpening.go
@@ -8,6 +8,18 @@ import (
 	"github.com/ojCezarFerreira/goportunities/schemas"
 )
 
+// @BasePath /api/v1
+// @Summary Delete Opening
+// @Description Delete a opening
+// @Tags Openings
+// @Accept json
+// @Produce json
+// @Param id query string true "Opening identification"
+// @Success 200 {object} DeleteOpeningResponse
+// @Failure 400 {object} ErrorResponse
+// @Failure 400 {object} ErrorResponse
+// @Failure 500 {object} ErrorResponse
+// @Router /opening [delete]
 func DeleteOpeningHandler(c *gin.Context) {
 	id := c.Query("id")
 	if id == "" {
