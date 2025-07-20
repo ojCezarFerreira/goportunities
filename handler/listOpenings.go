958e20aebbea3b3073b14264bf1cb7fad8b8cb06
Julio Cezar Leão Ferreira <cezarleferr@gmail.com>
2025-07-20 15:07:16 -0400

    docs: add documentation for list openings

--- handler/listOpenings.go
+++ handler/listOpenings.go
@@ -7,6 +7,15 @@ import (
 	"github.com/ojCezarFerreira/goportunities/schemas"
 )
 
+// @BasePath /api/v1
+// @Summary List Openings
+// @Description List all Openings
+// @Tags Openings
+// @Accept json
+// @Produce json
+// @Success 200 {object} ListOpeningsResponse
+// @Failure 500 {object} ErrorResponse
+// @Router /openings [get]
 func ListOpeningHandler(c *gin.Context) {
 	openings := []schemas.Opening{}
 
